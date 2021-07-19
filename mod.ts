5ea1b0273b70881b41d0017656ee2fca0213c704
Sean aye <hello@seanaye.ca>
2021-07-19 17:12:17 -0400

    export enum

--- mod.ts
+++ mod.ts
@@ -3,3 +3,5 @@ export type { StripeClient } from "./stripe.ts"
 
 export { getAuth0Client } from "./auth0.ts"
 export type { Auth0Client } from "./auth0.ts"
+
+export { SessionMode, PaymentMethodTypes } from "./stripe.types.ts"
