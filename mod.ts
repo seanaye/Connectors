1575ef9c1f36c94d508c4723e1e834d84dcc8000
Sean aye <hello@seanaye.ca>
2021-07-27 09:13:52 -0400

    update module exports

--- mod.ts
+++ mod.ts
@@ -4,5 +4,4 @@ export type { StripeClient } from "./stripe.ts"
 export { getAuth0Client } from "./auth0.ts"
 export type { Auth0Client } from "./auth0.ts"
 
-export { SessionMode, PaymentMethodTypes } from "./stripe.types.ts"
 export type { ReturnValue } from "./_utils.ts"
